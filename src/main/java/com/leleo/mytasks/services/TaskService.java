39b8fc4535871935ba7eeb17a3039607ad23e131
Leandro <leandroroncatto18@gmail.com>
2025-11-03 17:56:51 -0300

    feat: add create task service

--- src/main/java/com/leleo/mytasks/services/TaskService.java
+++ src/main/java/com/leleo/mytasks/services/TaskService.java
@@ -1,5 +1,7 @@
 package com.leleo.mytasks.services;
 
+import com.leleo.mytasks.dto.TaskRequest;
+import com.leleo.mytasks.model.Tag;
 import com.leleo.mytasks.model.Task;
 import com.leleo.mytasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
@@ -26,4 +29,25 @@ public class TaskService {
         return taskRepository.findById(id).orElseThrow();
     }
 
+    public Task createTask(TaskRequest request) {
+
+        Task task = new Task();
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setPriority(request.getPriority());
+        task.setCompleted(false);
+        task.setDate(request.getDate());
+
+        List<Tag> tags = request.getTags().stream().map(tr -> {
+            Tag tag = new Tag();
+            tag.setText(tr.getText());
+            tag.setColor(tr.getColor());
+            tag.setTask(task);
+            return tag;
+        }).collect(Collectors.toList());
+
+        task.setTags(tags);
+
+        return taskRepository.save(task);
+    }
 }
