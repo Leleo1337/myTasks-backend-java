54e8f1988edacda7b7b8a0b6d053f714ec8c4d5c
Leandro <leandroroncatto18@gmail.com>
2025-11-03 17:55:16 -0300

    feat: add create task route

--- src/main/java/com/leleo/mytasks/controllers/TaskController.java
+++ src/main/java/com/leleo/mytasks/controllers/TaskController.java
@@ -1,17 +1,19 @@
 package com.leleo.mytasks.controllers;
 
+import com.leleo.mytasks.dto.TaskRequest;
+import com.leleo.mytasks.model.Tag;
 import com.leleo.mytasks.model.Task;
+import com.leleo.mytasks.repository.TaskRepository;
 import com.leleo.mytasks.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api/v1/tasks") // define o prefixo para todos os endpoints
@@ -24,11 +26,17 @@ public class TaskController {
     public ResponseEntity<List<Task>> getAllTasks() {
         List<Task> tasks = taskService.getAllTasks();
         return ResponseEntity.ok(tasks);
-    };
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable UUID id) {
         Task task = taskService.getTask(id);
         return ResponseEntity.ok(task);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody TaskRequest request) {
+        Task createdTask = taskService.createTask(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 }
