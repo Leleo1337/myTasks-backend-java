52abad50e693b66391d196f9b605ffff0b9fbe0e
Leandro <leandroroncatto18@gmail.com>
2025-11-03 17:56:29 -0300

    chore: clean up code formatting

--- src/main/java/com/leleo/mytasks/MytasksApplication.java
+++ src/main/java/com/leleo/mytasks/MytasksApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MytasksApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MytasksApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MytasksApplication.class, args);
+    }
 }
