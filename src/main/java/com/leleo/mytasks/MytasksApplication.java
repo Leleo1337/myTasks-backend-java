e994dd1ec94fd677cc1a040d3cb1c9e28ffa5dee
Leandro <leandroroncatto18@gmail.com>
2025-11-06 19:22:26 -0300

    chore: fix line formats

--- src/main/java/com/leleo/mytasks/MytasksApplication.java
+++ src/main/java/com/leleo/mytasks/MytasksApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    MytasksApplication {
+public class MytasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(MytasksApplication.class, args);
     }
